92cc4bbaabe3fe99449182932f78a9019b51ecdd
joaopcbertoldo <joaopcbertoldo@gmail.com>
2020-10-14 15:27:44 +0200

    install pydot graphviz and update reqs

--- tomo2seg/data.py
+++ tomo2seg/data.py
@@ -14,29 +14,40 @@ models_dir.mkdir(exist_ok=True)
 
 
 class VolumePaths(namedtuple("VolumePaths", ["volume_name"])):
+
+    @property
     def train_data_path(self) -> Path:
         return data_dir / f"train.{self.volume_name}.raw"
 
+    @property
     def train_info_path(self) -> Path:
         return data_dir / f"train.{self.volume_name}.raw.info"
 
+    @property
     def train_labels_path(self) -> Path:
         return data_dir / f"train.labels.{self.volume_name}.raw"
 
+    @property
     def train_weights_path(self) -> Path:
         return data_dir / f"train.weights.{self.volume_name}.raw"
 
+    @property
     def val_data_path(self) -> Path:
         return data_dir / f"val.{self.volume_name}.raw"
 
+    @property
     def val_info_path(self) -> Path:
         return data_dir / f"val.{self.volume_name}.raw.info"
 
+    @property
     def val_labels_path(self) -> Path:
         return data_dir / f"val.labels.{self.volume_name}.raw"
 
+    @property
     def val_weights_path(self) -> Path:
         return data_dir / f"val.weights.{self.volume_name}.raw"
+
+    # todo check missing files?
 
 
 # VOLUMES
@@ -46,21 +57,27 @@ volume_precipitates_dryrun = VolumePaths(VOLUME_PRECIPITATES_DRYRUN)
 
 class ModelPaths(namedtuple("ModelPaths", ["model_name"])):
 
+    @property
     def model_path(self) -> Path:
         return models_dir / f"{self.model_name}"
 
+    @property
     def autosaved_model_path(self) -> Path:
         return models_dir / f"{self.model_name}-autosaved"
 
+    @property
     def logger_path(self) -> Path:
-        return self.model_path() / "logger.csv"
+        return self.model_path / "logger.csv"
 
+    @property
     def history_path(self) -> Path:
-        return self.model_path() / "history.csv"
+        return self.model_path / "history.csv"
 
+    @property
     def summary_path(self) -> Path:
-        return self.model_path() / "summary.txt"
+        return self.model_path / "summary.txt"
 
+    @property
     def architecture_plot_path(self) -> Path:
-        return self.model_path() / "architecture.png"
+        return self.model_path / "architecture.png"
 
