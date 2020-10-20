37c6b4b0db8afb9ec12d949bd039fb908ce50bcc
joaopcbertoldo <joaopcbertoldo@gmail.com>
2020-10-20 17:05:50 +0200

    modify update.sh to get the hostname

--- reqs/update.sh
+++ reqs/update.sh
@@ -3,8 +3,8 @@ project_root="$HOME/projects/tomo2seg"
 cd $project_root
 conda activate ${project_root}/condaenv
 
-conda env export > reqs/conda-env.yml
-conda env export --from-history > reqs/conda-env-hist.yml
-conda list > reqs/conda-list.txt
-conda list --explicit > reqs/conda-list-explicit.txt
-pip freeze > reqs/requirements-conda.txt
+conda env export > reqs/`hostname`.conda-env.yml
+conda env export --from-history > reqs/`hostname`.conda-env-hist.yml
+conda list > reqs/`hostname`.conda-list.txt
+conda list --explicit > reqs/`hostname`.conda-list-explicit.txt
+pip freeze > reqs/`hostname`.recoquirements-conda.txt
