95a4c436692254f64764b5da361b5673bdb7e3cb
joaopcbertoldo <24547377+joaopcbertoldo@users.noreply.github.com>
2021-03-29 11:43:29 +0200

    update deps

--- reqs/savedeps.sh
+++ reqs/savedeps.sh
@@ -4,16 +4,28 @@ project_env="tomo2seg-dev"
 cd "${project_root}/reqs" || exit
 conda activate $project_env
 
+{ python -V & conda -V; } > python-and-conda-versions.txt
+
 conda env export --from-history > conda-env-hist.yml
 conda env export > conda-env.yml
 conda list > conda-list.txt
 conda list --explicit > conda-list-explicit.txt
 pip freeze > requirements.txt
 
-# list of things installed with pip after conda
-#- adabelief_tf
+cd "${project_root}" || exit
 
 
 # installation commands that I ran
+
 # conda install --yes -c conda-forge tensorflow-gpu pyyaml humanize pandas matplotlib seaborn imageio pydot graphviz  progressbar2 tqdm scikit-learn
-# pip install adabelief_tf
+
+# list of things installed with pip after conda
+# - adabelief_tf
+# - pre-commit
+# - ipython
+# - ipykernel
+
+# !!!!!!!!!!!!!!!!!!!!!!!! i should install `ipython ipykernel` as well
+
+
+# conda develop .
