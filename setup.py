9be5332a60c5c707c64f79617db6c99541650586
joaopcbertoldo <24547377+joaopcbertoldo@users.noreply.github.com>
2021-03-29 12:07:17 +0200

    modif setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,6 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 repo_name = "tomo2seg"
 
-cnn_segm_path = str((here / "cnn_segm").absolute())
-cnn_segm_packages = [
-    f"{cnn_segm_path}/{package}" for package in find_packages(where=cnn_segm_path)
-]
-
 setup(
     name=repo_name,
     version="0.0.0",
@@ -30,37 +25,37 @@ setup(
         "Operating System :: OS Independent",
     ],
     keywords="",
-    packages=find_packages(where=repo_name) + cnn_segm_packages,
+    packages=find_packages(where=repo_name),
     python_requires=">=3.6.8",
     install_requires=[
         # todo update versions with those from conda?
-        "numpy==1.18.5",  # it must be < 1.19.0 because of an incompatibility with tensorflow
-        "scikit-learn==0.23.2",
-        "scikit-image==0.17.2",
-        "pandas==1.1.0",
-        "jupyterlab==2.2.4",
-        "jupyter_contrib_nbextensions==0.5.1",
-        "matplotlib==3.3.0",
-        "seaborn==0.10.1",
-        "tensorflow==2.3.1",  # install tensorflow-gpu instead!
-        "pymicro==0.4.5",
-        "tensorboard==2.3.0",
-        # "wandb==0.10.2",  # i cannot install this because it depends on psutil, which can only be installed with python3-devel, which i cannot install without sudo...
-        "scipy== 1.5.2",
-        "streamlit==0.67.1",
-        "Keras==2.4.3",
-        "imageio==2.5.0",
-        "opencv-python==4.1.0.25",
-        "pytest==5.4.1",
-        "humanize==3.0.1",
+        # "numpy==1.18.5",  # it must be < 1.19.0 because of an incompatibility with tensorflow
+        # "scikit-learn==0.23.2",
+        # "scikit-image==0.17.2",
+        # "pandas==1.1.0",
+        # "jupyterlab==2.2.4",
+        # "jupyter_contrib_nbextensions==0.5.1",
+        # "matplotlib==3.3.0",
+        # "seaborn==0.10.1",
+        # "tensorflow==2.3.1",  # install tensorflow-gpu instead!
+        # "pymicro==0.4.5",
+        # "tensorboard==2.3.0",
+        # # "wandb==0.10.2",  # i cannot install this because it depends on psutil, which can only be installed with python3-devel, which i cannot install without sudo...
+        # "scipy== 1.5.2",
+        # "streamlit==0.67.1",
+        # "Keras==2.4.3",
+        # "imageio==2.5.0",
+        # "opencv-python==4.1.0.25",
+        # "pytest==5.4.1",
+        # "humanize==3.0.1",
     ],
     extras_require={
         "dev": [
-            "pre-commit==2.6.0",
-            "flake8==3.8.3",
-            "flake8-bugbear==20.1.4",
-            "black==19.10b0",
-            "isort==5.3.2",
+            # "pre-commit==2.6.0",
+            # "flake8==3.8.3",
+            # "flake8-bugbear==20.1.4",
+            # "black==19.10b0",
+            # "isort==5.3.2",
         ],
     },
 )
